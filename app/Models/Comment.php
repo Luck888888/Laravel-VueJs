1d5ab48dd5af80cad15e0db36a06fa8d12b781cb
Tatsiana <t-tanja@bk.ru>
2021-07-13 15:12:35 +0300

    init: start laravelvueJs

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $fillable=['subject','body','article_id'];
+
+    //комментарий относится к статье (инверсия связи один ко многим)
+    public function article(){
+        return $this->belongsTo(Article::class);
+    }
+}
